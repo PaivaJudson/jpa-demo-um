671e9bfc0159f6849ffe11002ae43247a5859d15
judsonpaiva <judson.paiva@hotmail.com>
2022-06-02 22:18:16 +0100

    classe Cliente, atributos e métodos

--- src/main/java/com/jpa/cadastrocliente/model/Cliente.java
+++ src/main/java/com/jpa/cadastrocliente/model/Cliente.java
@@ -1,2 +1,38 @@
-package com.jpa.cadastrocliente.model;public class Cliente {
+package com.jpa.cadastrocliente.model;
+
+import java.util.Objects;
+
+public class Cliente {
+
+    private Integer id;
+    private String nome;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return id.equals(cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
